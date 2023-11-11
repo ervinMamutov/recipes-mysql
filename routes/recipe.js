2e85cfa9b56c94bb5baad8187e3baec76511a8fb
ervinMamutov <ervin.news@gmail.com>
2023-11-11 15:52:59 +0100

    added routes recipe

--- routes/recipe.js
+++ routes/recipe.js
@@ -6,6 +6,10 @@ import recipeControllers from '../controllers/recipe.js';
 import verifyToken from '../middleware/verifyToken.js';
 
 // routes
-
+router.get('/', recipeControllers.getRecipes);
+router.get('/:id', recipeControllers.getRecipe);
+router.post('/add-recipe', verifyToken, recipeControllers.addRecipe);
+router.put('/update/:id', verifyToken, recipeControllers.updateRecipe);
+router.delete('/delete/:id', verifyToken, recipeControllers.deleteRecipe);
 
 export default router;
