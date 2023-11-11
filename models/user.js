7ef9a662ce84af31c9fd4b318acd332206f1f35e
ervinMamutov <ervin.news@gmail.com>
2023-11-11 15:50:10 +0100

    added model user

--- models/user.js
+++ models/user.js
@@ -1,8 +1,18 @@
 import query from '../config/db.js';
 
 const createUser = async () => {
-    try {
-    } catch (err) {}
+  try {
+    const createUserTable = `
+    CREATE TABLE IF NOT EXISTS users (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      email VARCHAR(50) NOT NULL UNIQUE,
+      password TEXT NOT NULL
+    );`;
+    const result = await query(createUserTable);
+    console.log(`The user table created successfully`);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 createUser();
