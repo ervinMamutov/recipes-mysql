58115485fe18a430da2c1058b6aed5fc5f0f58cc
ervinMamutov <ervin.news@gmail.com>
2023-11-11 15:50:45 +0100

    added model recipe

--- models/recipe.js
+++ models/recipe.js
@@ -1,8 +1,18 @@
 import query from '../config/db.js';
 
 const createRecipe = async () => {
-    try {
-    } catch (err) {}
+  try {
+    const createRecipeTable = `
+    CREATE TABLE IF NOT EXISTS recipes (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      name VARCHAR(50) NOT NULL,
+      description TEXT NOT NULL
+    );`;
+    const result = await query(createRecipeTable);
+    console.log('Recipe tables create successfully');
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 createRecipe();
