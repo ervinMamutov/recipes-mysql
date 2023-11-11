96f5975ff227c4ba7ae0e860eaa6c9971c340bcb
ervinMamutov <ervin.news@gmail.com>
2023-11-11 15:55:02 +0100

    added config db

--- config/db.js
+++ config/db.js
@@ -9,28 +9,28 @@ const PATH = dirname(__filename);
 dotenv.config({ path: path.join(PATH, '../.env') });
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: 10
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 10
 });
 
 console.log('MySQL Pool created successfully');
 
-// Create query
+// create query
 const query = async (sql, values) => {
-    const connection = await pool.getConnection();
-    try {
-        const [results] = await connection.query(sql, values);
-        return results;
-    } catch (err) {
-        return err;
-    } finally {
-        if (connection) {
-            connection.release();
-        }
+  const connection = await pool.getConnection();
+  try {
+    const [results] = await connection.query(sql, values);
+    return results;
+  } catch (err) {
+    return err;
+  } finally {
+    if (connection) {
+      connection.release();
     }
+  }
 };
 
 export default query;
